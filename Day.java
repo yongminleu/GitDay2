a6ea4669a640a37ab4bfe0b187ecaab21dafd82e
Yongmin Leu <yleu66@gmail.com>
2023-03-28 19:50:41 -0400

    Update3

--- Day.java
+++ Day.java
@@ -2,5 +2,6 @@ public class Day {
     public static void main(String[] args) {
         System.out.println("1111111");
         System.out.println("1111111");
+        System.out.println("1111111");
     }
 }
